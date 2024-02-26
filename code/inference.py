94fbf0ddda8aeac6e13b6746c848bbf3f5649191
adnankarol <adnankarol@MacBook-Pro-Adnan.local>
2024-02-26 11:04:41 +0100

    Update inference

--- code/inference.py
+++ code/inference.py
@@ -7,8 +7,7 @@ __status__ = "Dev"
 
 # Import Dependencies
 import numpy as np
-import torch, os, json
-import io
+import torch
 import cv2
 import os
 import json
@@ -16,19 +15,19 @@ from ultralytics import YOLO
 
 
 def model_fn(model_dir):
-    model = YOLO("yolov8l.pt'")
+    model_path = os.path.join(model_dir, "yolov8l.pt")
+    model = YOLO(model_path)
     return model
 
+
 def input_fn(request_body, request_content_type):
-    if request_content_type:
-        jpg_original = np.load(io.BytesIO(request_body),
-                               allow_pickle=True)
-        jpg_as_np = np.frombuffer(request_body, 
-		                          dtype=np.uint8)
+    if request_content_type == "image/jpeg":
+        jpg_as_np = np.frombuffer(request_body, dtype=np.uint8)
         img = cv2.imdecode(jpg_as_np, flags=-1)
+        return img
     else:
         raise Exception("Unsupported content type: " + request_content_type)
-    return img
+    
 
 def predict_fn(input_data, model):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -37,13 +36,15 @@ def predict_fn(input_data, model):
         result = model(input_data)
     return result
 
-def output_fn(prediction_output, content_type):
+
+def output_fn(prediction_output, accept):
     infer = {}
     for result in prediction_output:
         if result.boxes:
-            infer['boxes'] = result.boxes.numpy().data.tolist()
+            infer['boxes'] = result.boxes.cpu().numpy().tolist()
         if result.masks:
-            infer['masks'] = result.masks.numpy().data.tolist()
+            infer['masks'] = result.masks.cpu().numpy().tolist()
         if result.probs:
-            infer['probs'] = result.probs.numpy().data.tolist()
+            infer['probs'] = result.probs.cpu().numpy().tolist()
     return json.dumps(infer)
+
